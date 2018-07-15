29396d33b58c19da3780c8da16604709e14c19d1
ybangera <yatinbangera@gmail.com>
2018-07-15 17:56:31 -0400

    added reducers and containers

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>Redux App 1.0</div>
+      <div>
+        <BookList/>
+      </div>
+
     );
   }
 }
